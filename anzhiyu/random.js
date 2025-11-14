2e9fa8c1225f6a1e32e0be40a85a890926084159
第二心跳 <915643957@qq.com>
2025-11-14 16:14:55 +0800

    Site updated: 2025-11-14 16:14:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/14/hello-world/","2025/11/14/测试题/","2025/11/14/我的第一个博客/","2025/11/14/第二个文章/"];function toRandomPost(){
+var posts=["2025/11/14/hello-world/","2025/11/14/我的第一个博客/","2025/11/14/测试题/","2025/11/14/第二个文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
